845928b0615a37d94a6a11b9a3703d179f4234c9
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-04-09 15:23:52 +0200

    Configure our application to work with a Firebase back-end API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAKJFa_Y4K-SxTPovYmoCvprGCWRQoDvMI',
+    authDomain: 'angular-notes-app-c4444.firebaseapp.com',
+    databaseURL: 'https://angular-notes-app-c4444.firebaseio.com',
+    projectId: 'angular-notes-app-c4444',
+    storageBucket: '',
+    messagingSenderId: '741252662968'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAKJFa_Y4K-SxTPovYmoCvprGCWRQoDvMI',
+    authDomain: 'angular-notes-app-c4444.firebaseapp.com',
+    databaseURL: 'https://angular-notes-app-c4444.firebaseio.com',
+    projectId: 'angular-notes-app-c4444',
+    storageBucket: '',
+    messagingSenderId: '741252662968'
+  }
 };
